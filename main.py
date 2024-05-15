2fe824fcf5099d36575dbce9f64ff144c7e8edd1
7ahin <97078768+7ahin@users.noreply.github.com>
2024-05-15 09:27:58 +0800

    Add files via upload

---
+++ main.py
@@ -0,0 +1,9 @@
+import warnings
+warnings.filterwarnings("ignore", category=FutureWarning)
+import pandas as pd
+import numpy as np
+import matplotlib.pyplot as plt
+import seaborn as sns
+from scipy.sparse import csr_matrix
+from recommeders.knn_recommender import Recommender
+
