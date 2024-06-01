6168025206af89893fd57e6dfbabcbb61f403fbd
BeemBop <136294826+BeemBop@users.noreply.github.com>
2024-06-01 17:11:50 +0800

    Update main.py

--- main.py
+++ main.py
@@ -1,23 +1,25 @@
 import pandas as pd
 import numpy as np
-
 from sklearn.metrics.pairwise import linear_kernel
 from sklearn.feature_extraction.text import TfidfVectorizer
 
 # Load the datasets
-song_df = pd.read_csv('animesongsemotion.csv')
+song_df = pd.read_csv('/animesongsemotion.csv')
 
 # TF-IDF Vectorization on emotion
 tfidf = TfidfVectorizer(stop_words='english')
-tfidf_matrix = tfidf.fit_transform(song_df['emotions'])
+tfidf_matrix = tfidf.fit_transform(song_df['Emotions'])
 
 # Compute the cosine similarity matrix using linear_kernel
 cosine_similarity = linear_kernel(tfidf_matrix, tfidf_matrix)
 
 # Function to get song recommendations based on song emotion
-def get_recommendations(emotions):
+def get_recommendations(emotion):
+    # Ensure the emotion is lowercased for comparison
+    emotion = emotion.lower()
+    
     # Find the indices of songs with the specified emotion
-    emotion_indices = song_df[song_df['emotion'].str.lower() == emotion].index
+    emotion_indices = song_df[song_df['Emotions'].str.lower() == emotion].index
     if len(emotion_indices) == 0:
         return "No songs found for the specified emotion."
     
@@ -26,7 +28,7 @@ def get_recommendations(emotions):
     similar_songs = list(enumerate(cosine_similarity[emotion_index]))
     similar_songs = sorted(similar_songs, key=lambda x: x[1], reverse=True)[1:6]
 
-    recommendations = [song_df.iloc[i[0]]['song'] for i in similar_songs]
+    recommendations = [song_df.iloc[i[0]]['Song Name '] for i in similar_songs]
     return recommendations
 
 # Example usage
